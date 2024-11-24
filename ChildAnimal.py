35bae6f49f577208956506b1b007d2996752aa5b
Kajhoni <kajhon.itdhanuvekin@baruchmail.cuny.edu>
2024-11-24 13:10:33 -0500

    Create ChildAnimal.py

---
+++ ChildAnimal.py
@@ -0,0 +1,9 @@
+from Animal import Animal
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def speak(self):
+        return "Woof!"
